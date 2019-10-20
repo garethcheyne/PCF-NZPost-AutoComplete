7e234206a6da49f3e7511bbd7a15aff9ac863986
garethcheyne <gareth.cheyne@nz.harveynorman.com>
2019-10-20 13:36:38 +1300

    update

--- NZPOSTAutoComplete/index.ts
+++ NZPOSTAutoComplete/index.ts
@@ -1,4 +1,7 @@
 import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { threadId } from "worker_threads";
+import { rejects } from "assert";
+import { string } from "prop-types";
 
 
 export class NZPostAutoComplete implements ComponentFramework.StandardControl<IInputs, IOutputs> {
@@ -14,9 +17,18 @@ export class NZPostAutoComplete implements ComponentFramework.StandardControl<II
 	// Datalist element.
 	private datalistElement: HTMLDataListElement;
 
-	private nzpost_client_id: string | null;
-	private nzpost_client_secret: string | null;
+	private clientId: string | null;
+	private clientSecret: string | null;
 
+	private _street: string;
+	private _building: string;
+	private _suburb: string;
+	private _postcode: string;
+	private _city: string;
+	private _country: string;
+	private _dpid: string;
+	private _longitude: number;
+	private _latitude: number;
 	private id: string;
 
 	constructor()
@@ -34,12 +46,11 @@ export class NZPostAutoComplete implements ComponentFramework.StandardControl<II
 	 */
 	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
 	{
-
 		this.localNotifyOutputChanged = notifyOutputChanged;
 		this.context = context;
-		this.nzpost_client_id = context.parameters.nzpost_client_id.raw;
-		this.nzpost_client_secret = context.parameters.nzpost_client_secret.raw;
-		
+		this.clientId = context.parameters.nzpost_client_id.raw;
+		this.clientSecret = context.parameters.nzpost_client_secret.raw;
+	
 		// @ts-ignore
 		this.id = context.parameters.value.attributes.LogicalName;
 
@@ -56,7 +67,8 @@ export class NZPostAutoComplete implements ComponentFramework.StandardControl<II
 
 		// Get initial values from field.
 		// @ts-ignore
-		this.inputElement.value = Xrm.Page.getAttribute(context.parameters.value.attributes.LogicalName).getValue();
+		this.inputElement.value = this.context.parameters.value.formatted;
+		//this.inputElement.value = Xrm.Page.getAttribute(context.parameters.value.attributes.LogicalName).getValue();
 
 		// Add an eventlistner the element and bind it to a  function.
 		this.inputElement.addEventListener("input", this.getSuggestions.bind(this));
@@ -88,7 +100,7 @@ export class NZPostAutoComplete implements ComponentFramework.StandardControl<II
 		if (input.indexOf(key) == -1)
 		{
 			this.datalistElement.innerHTML = "";
-			let query = "/suggest?q=" + input + "&type=All&max=20&client_id=" + this.nzpost_client_id + "&client_secret=" + this.nzpost_client_secret;
+			let query = "/suggest?q=" + input + "&type=All&max=20&client_id=" + this.clientId + "&client_secret=" + this.clientSecret;
 			let options = {
 				host: 'api.nzpost.co.nz/privateaddresschecker/1.0',
 				path: query,
@@ -118,7 +130,7 @@ export class NZPostAutoComplete implements ComponentFramework.StandardControl<II
 						optionsHTMLArray.push('">  Address Type: ' + response.addresses[i].SourceDesc + '</option>');						
 					}
 					this.datalistElement.innerHTML = optionsHTMLArray.join("");  
-					this.localNotifyOutputChanged  
+					//this.localNotifyOutputChanged  
 				});
 
 			}).on("error", (err: { message: string; }) => {
@@ -126,7 +138,8 @@ export class NZPostAutoComplete implements ComponentFramework.StandardControl<II
 			});		
 		}
 		else {
-			this.localNotifyOutputChanged()
+			this.getDetails(this.inputElement.value)
+			
 		}		
 	}
 
@@ -134,16 +147,22 @@ export class NZPostAutoComplete implements ComponentFramework.StandardControl<II
 	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
 	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
 	 */
+	
+
 	public updateView(context: ComponentFramework.Context<IInputs>): void
 	{
-		console.log("updateView");
-		console.log("setSelected");
+		((this._street != undefined) ? this.inputElement.value = this._street : null)
+	}
+	
+
+	public getDetails(value: string){
+
 		// set the key to lok for in the input that was placed above.
-		let key = "DPID"
-		let value = this.inputElement.value
+		console.log("getDetails");
+		let key = "DPID";
 		if (value.indexOf(key) > -1){
 			let _dpid = value.substring(value.indexOf(key) + 6, value.length);
-			var query = "/details?type=All&dpid=" + _dpid + "&max=20&client_id=" + this.nzpost_client_id + "&client_secret=" + this.nzpost_client_secret;
+			var query = "/details?type=All&dpid=" + _dpid + "&max=20&client_id=" + this.clientId + "&client_secret=" + this.clientSecret;
 			var options = {
 				host: 'api.nzpost.co.nz/privateaddresschecker/1.0',
 				path: query,
@@ -151,66 +170,42 @@ export class NZPostAutoComplete implements ComponentFramework.StandardControl<II
 					'accept': 'application/json'
 				}
 			}
-	
-			const https = require('https');
 
+			const https = require('https');
 			https.get(options, (resp: any) => {
-				let data = '';
-				
+				let data = '';				
 				// A chunk of data has been recieved.
 				resp.on('data', (chunk: any) => {
-				  data += chunk;
-				});
-			  
+					data += chunk;
+				});				
 				// The whole response has been received. Print out the result.
 				resp.on('end', () => {
 					var response = JSON.parse(data);
-					console.log("setAddress API response")
-		
+					console.log("setAddress API response")			
 					if (response.success){
-						console.log(response);
-						this.inputElement.value = response.details[response.details.length -1].AddressLine1
-						// @ts-ignore
-						Xrm.Page.getAttribute(context.parameters.address_street.attributes.LogicalName).setValue(response.details[response.details.length -1].AddressLine1);
-						// @ts-ignore
-						Xrm.Page.getAttribute(context.parameters.address_building.attributes.LogicalName).setValue(response.details[response.details.length -1].AddressLine5);
-						// @ts-ignore
-						Xrm.Page.getAttribute(context.parameters.address_suburb.attributes.LogicalName).setValue(response.details[response.details.length -1].Suburb);
-						// @ts-ignore
-						Xrm.Page.getAttribute(context.parameters.address_postcode.attributes.LogicalName).setValue(response.details[response.details.length -1].Postcode);
-						// @ts-ignore
-						Xrm.Page.getAttribute(context.parameters.address_city.attributes.LogicalName).setValue(response.details[response.details.length -1].CityTown);
-						// @ts-ignore
-						Xrm.Page.getAttribute(context.parameters.address_country.attributes.LogicalName).setValue("New Zealand");
-						// @ts-ignore
-						if (context.parameters.address_longitude.attributes.LogicalName.raw != undefined)
-						{
-							// @ts-ignore
-							Xrm.Page.getAttribute(context.parameters.address_longitude.attributes.LogicalName).setValue(response.details[response.details.length -1].NZGD2kCoord.coordinates[0]);				
-						}
-						// @ts-ignore
-						if (context.parameters.address_latitude.attributes.LogicalName.raw != undefined)
-						{
-							// @ts-ignore
-							Xrm.Page.getAttribute(context.parameters.address_latitude.attributes.LogicalName).setValue(response.details[response.details.length -1].NZGD2kCoord.coordinates[1]);				
-						}
-						// @ts-ignore
-						if (context.parameters.address_dpid.attributes.LogicalName.raw != undefined)
-						{
-							// @ts-ignore
-							Xrm.Page.getAttribute(context.parameters.address_dpid.attributes.LogicalName).setValue(response.details[response.details.length -1].DPID);				
-						}					
+						console.log(response)
+						this._street = response.details[response.details.length -1].AddressLine1;
+						//this._building =  response.details[response.details.length -1]??;
+						this._suburb =  response.details[response.details.length -1].Suburb;
+						this._postcode =  response.details[response.details.length -1].Postcode;
+						this._city = response.details[response.details.length -1].CityTown;
+						this._country =  "New Zealand";
+						this._latitude = response.details[response.details.length -1].NZGD2kCoord.coordinates[0];
+						this._longitude = response.details[response.details.length -1].NZGD2kCoord.coordinates[1];
+						this._dpid = response.details[response.details.length -1].DPID;
+						this.localNotifyOutputChanged()			
 					}
 				});
 
-			}).on("error", (err: { message: string; }) => {
-				console.log("Error: " + err.message);
-			});		
-		}
-		else {
-			console.log("setSelected :: No " + key);
-		}		
+				}).on("error", (err: { message: string; }) => {
+					console.log("Error: " + err.message);			
+				});						
+			}
+			else {
+				console.log("setSelected :: No " + key);
+			}	
 	}
+
 
 	/** 
 	 * It is called by the framework prior to a control receiving new data. 
@@ -218,9 +213,19 @@ export class NZPostAutoComplete implements ComponentFramework.StandardControl<II
 	 */
 	public getOutputs(): IOutputs
 	{	
-		console.log("getOutput");
-		this.updateView(this.context)
-		return {};
+
+		return {
+			value: this._street,
+			address_street: this._street,
+			address_building: this._building,
+			address_suburb: this._suburb,
+			address_postcode: this._postcode,
+			address_city: this._city,
+			address_country: this._country,
+			address_dpid: this._dpid
+			// address_latitude: this._latitude,
+			// address_longitude: this._longitude
+		};
 	}
 
 	/** 
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 
-### PCF NZ Post Autocomplete - αlpha  
+# PCF-NZ-Post-AutoComplete
+
+Dynamics365 / PowerApps PCF Control to consume the API from NZ Post.
 
 AutoComplete function for NZ Post API. You first will need to obtain an client_id, and client_secret from NZPost, https://anypoint.mulesoft.com/exchange/portals/nz-post-group/b8271f09-2ad8-4e1c-b6b1-322c5727d148/nzpost.addresschecker/
 
 
-Fell free to add and constructive criticism.
 
